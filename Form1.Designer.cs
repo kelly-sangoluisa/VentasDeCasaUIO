fd62c69bc4ff8732fb5ff4a40b7cd40a2d58d2f7
kelly-sangoluisa <94008979+kelly-sangoluisa@users.noreply.github.com>
2022-02-22 19:06:24 -0500

    agregracion de boton comprar y confirmar

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,67 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblBienvenidos = new System.Windows.Forms.Label();
+            this.txtPresupuesto = new System.Windows.Forms.TextBox();
             this.lblCompra = new System.Windows.Forms.Label();
             this.txtNombre = new System.Windows.Forms.TextBox();
             this.lblPresupuesto = new System.Windows.Forms.Label();
-            this.txtPresupuesto = new System.Windows.Forms.TextBox();
+            this.lblBienvenidos = new System.Windows.Forms.Label();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.btnComprar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // lblBienvenidos
+            // txtPresupuesto
             // 
-            this.lblBienvenidos.AutoSize = true;
-            this.lblBienvenidos.Font = new System.Drawing.Font("Script MT Bold", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBienvenidos.Location = new System.Drawing.Point(280, 32);
-            this.lblBienvenidos.Name = "lblBienvenidos";
-            this.lblBienvenidos.Size = new System.Drawing.Size(334, 28);
-            this.lblBienvenidos.TabIndex = 0;
-            this.lblBienvenidos.Text = "Bienvenidos a ventas de casas UIO";
-            this.lblBienvenidos.Click += new System.EventHandler(this.label1_Click);
+            this.txtPresupuesto.Location = new System.Drawing.Point(170, 116);
+            this.txtPresupuesto.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPresupuesto.Name = "txtPresupuesto";
+            this.txtPresupuesto.Size = new System.Drawing.Size(152, 20);
+            this.txtPresupuesto.TabIndex = 4;
             // 
             // lblCompra
             // 
             this.lblCompra.AutoSize = true;
-            this.lblCompra.Location = new System.Drawing.Point(69, 100);
+            this.lblCompra.Location = new System.Drawing.Point(52, 81);
+            this.lblCompra.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCompra.Name = "lblCompra";
-            this.lblCompra.Size = new System.Drawing.Size(114, 16);
+            this.lblCompra.Size = new System.Drawing.Size(92, 13);
             this.lblCompra.TabIndex = 1;
             this.lblCompra.Text = "Ingresa tu nombre";
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(226, 97);
+            this.txtNombre.Location = new System.Drawing.Point(170, 79);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(2);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(202, 22);
+            this.txtNombre.Size = new System.Drawing.Size(152, 20);
             this.txtNombre.TabIndex = 2;
             // 
             // lblPresupuesto
             // 
             this.lblPresupuesto.AutoSize = true;
-            this.lblPresupuesto.Location = new System.Drawing.Point(69, 149);
+            this.lblPresupuesto.Location = new System.Drawing.Point(52, 121);
+            this.lblPresupuesto.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPresupuesto.Name = "lblPresupuesto";
-            this.lblPresupuesto.Size = new System.Drawing.Size(143, 16);
+            this.lblPresupuesto.Size = new System.Drawing.Size(115, 13);
             this.lblPresupuesto.TabIndex = 3;
             this.lblPresupuesto.Text = "Ingresa tu presupuesto";
             // 
-            // txtPresupuesto
+            // lblBienvenidos
             // 
-            this.txtPresupuesto.Location = new System.Drawing.Point(226, 143);
-            this.txtPresupuesto.Name = "txtPresupuesto";
-            this.txtPresupuesto.Size = new System.Drawing.Size(202, 22);
-            this.txtPresupuesto.TabIndex = 4;
+            this.lblBienvenidos.AutoSize = true;
+            this.lblBienvenidos.Font = new System.Drawing.Font("Script MT Bold", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBienvenidos.Location = new System.Drawing.Point(210, 26);
+            this.lblBienvenidos.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblBienvenidos.Name = "lblBienvenidos";
+            this.lblBienvenidos.Size = new System.Drawing.Size(279, 23);
+            this.lblBienvenidos.TabIndex = 0;
+            this.lblBienvenidos.Text = "Bienvenidos a ventas de casas UIO";
+            this.lblBienvenidos.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(319, 574);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirmar.TabIndex = 5;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            // 
+            // btnComprar
+            // 
+            this.btnComprar.Location = new System.Drawing.Point(319, 454);
+            this.btnComprar.Name = "btnComprar";
+            this.btnComprar.Size = new System.Drawing.Size(75, 23);
+            this.btnComprar.TabIndex = 6;
+            this.btnComprar.Text = "Comprar";
+            this.btnComprar.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(123, 483);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(491, 85);
+            this.textBox1.TabIndex = 7;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(972, 827);
+            this.ClientSize = new System.Drawing.Size(729, 609);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnComprar);
+            this.Controls.Add(this.btnConfirmar);
             this.Controls.Add(this.txtPresupuesto);
             this.Controls.Add(this.lblPresupuesto);
             this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.lblCompra);
             this.Controls.Add(this.lblBienvenidos);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -98,11 +134,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblBienvenidos;
+        private System.Windows.Forms.TextBox txtPresupuesto;
         private System.Windows.Forms.Label lblCompra;
         private System.Windows.Forms.TextBox txtNombre;
         private System.Windows.Forms.Label lblPresupuesto;
-        private System.Windows.Forms.TextBox txtPresupuesto;
+        private System.Windows.Forms.Label lblBienvenidos;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Button btnComprar;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
