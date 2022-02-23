dc1ae59e0980be720f482bf093041b33943911a0
kelly-sangoluisa <94008979+kelly-sangoluisa@users.noreply.github.com>
2022-02-22 19:08:00 -0500

    boton

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,63 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtPresupuesto = new System.Windows.Forms.TextBox();
-            this.lblCompra = new System.Windows.Forms.Label();
-            this.txtNombre = new System.Windows.Forms.TextBox();
-            this.lblPresupuesto = new System.Windows.Forms.Label();
-            this.lblBienvenidos = new System.Windows.Forms.Label();
             this.btnConfirmar = new System.Windows.Forms.Button();
             this.btnComprar = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // txtPresupuesto
-            // 
-            this.txtPresupuesto.Location = new System.Drawing.Point(170, 116);
-            this.txtPresupuesto.Margin = new System.Windows.Forms.Padding(2);
-            this.txtPresupuesto.Name = "txtPresupuesto";
-            this.txtPresupuesto.Size = new System.Drawing.Size(152, 20);
-            this.txtPresupuesto.TabIndex = 4;
-            // 
-            // lblCompra
-            // 
-            this.lblCompra.AutoSize = true;
-            this.lblCompra.Location = new System.Drawing.Point(52, 81);
-            this.lblCompra.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblCompra.Name = "lblCompra";
-            this.lblCompra.Size = new System.Drawing.Size(92, 13);
-            this.lblCompra.TabIndex = 1;
-            this.lblCompra.Text = "Ingresa tu nombre";
-            // 
-            // txtNombre
-            // 
-            this.txtNombre.Location = new System.Drawing.Point(170, 79);
-            this.txtNombre.Margin = new System.Windows.Forms.Padding(2);
-            this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(152, 20);
-            this.txtNombre.TabIndex = 2;
-            // 
-            // lblPresupuesto
-            // 
-            this.lblPresupuesto.AutoSize = true;
-            this.lblPresupuesto.Location = new System.Drawing.Point(52, 121);
-            this.lblPresupuesto.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblPresupuesto.Name = "lblPresupuesto";
-            this.lblPresupuesto.Size = new System.Drawing.Size(115, 13);
-            this.lblPresupuesto.TabIndex = 3;
-            this.lblPresupuesto.Text = "Ingresa tu presupuesto";
-            // 
-            // lblBienvenidos
-            // 
-            this.lblBienvenidos.AutoSize = true;
-            this.lblBienvenidos.Font = new System.Drawing.Font("Script MT Bold", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBienvenidos.Location = new System.Drawing.Point(210, 26);
-            this.lblBienvenidos.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblBienvenidos.Name = "lblBienvenidos";
-            this.lblBienvenidos.Size = new System.Drawing.Size(279, 23);
-            this.lblBienvenidos.TabIndex = 0;
-            this.lblBienvenidos.Text = "Bienvenidos a ventas de casas UIO";
-            this.lblBienvenidos.Click += new System.EventHandler(this.label1_Click);
             // 
             // btnConfirmar
             // 
@@ -120,11 +67,6 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnComprar);
             this.Controls.Add(this.btnConfirmar);
-            this.Controls.Add(this.txtPresupuesto);
-            this.Controls.Add(this.lblPresupuesto);
-            this.Controls.Add(this.txtNombre);
-            this.Controls.Add(this.lblCompra);
-            this.Controls.Add(this.lblBienvenidos);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -133,12 +75,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox txtPresupuesto;
-        private System.Windows.Forms.Label lblCompra;
-        private System.Windows.Forms.TextBox txtNombre;
-        private System.Windows.Forms.Label lblPresupuesto;
-        private System.Windows.Forms.Label lblBienvenidos;
         private System.Windows.Forms.Button btnConfirmar;
         private System.Windows.Forms.Button btnComprar;
         private System.Windows.Forms.TextBox textBox1;
