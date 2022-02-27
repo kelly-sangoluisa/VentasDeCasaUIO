0f8bbdb9bd344d06a8488d9697c987ddf587460d
kelly-sangoluisa <94008979+kelly-sangoluisa@users.noreply.github.com>
2022-02-26 19:42:59 -0500

    declaracion de estructuras

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -402,6 +402,7 @@
             this.btnBuscar.TabIndex = 35;
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // rbtnNoEspaciosVerdes
             // 
